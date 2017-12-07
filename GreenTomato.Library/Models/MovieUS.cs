6805476d4eced0e8f53bb7f96e0e6cbd139fc5b4
fred belotte <fred@belotte.xyz>
2017-12-07 14:18:10 -0500

    add the movie region models.

---
+++ GreenTomato.Library/Models/MovieUS.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using GreenTomato.Library.Interfaces;
+
+namespace GreenTomato.Library.Models
+{
+  public class MovieUS : IMovie
+  {
+    public string Title { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public Genre Genre { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public List<Actor> Actors { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+  }
+}
