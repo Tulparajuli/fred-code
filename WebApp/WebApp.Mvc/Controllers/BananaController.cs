b964e74900c88a57721f0aebd09af4b442efe673
fred belotte <fred.belotte@revature.com>
2017-12-20 17:48:00 -0500

    add the mvc bits.

---
+++ WebApp/WebApp.Mvc/Controllers/BananaController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Mvc.Filters;
+using WebApp.Mvc.Models;
+
+namespace WebApp.Mvc.Controllers
+{
+  public class BananaController : Controller
+  {
+    // GET: Banana
+    //public ActionResult Index()
+    //{
+    //  var p = new Person() { Name = "fred" };
+    //  ViewBag.Data = "nada";
+    //  ViewData["Data"] = "niet";
+
+    //  TempData["Data"] = "fred";
+    //  Session["Data"] = "belotte";
+
+    //  return RedirectToAction("Default");
+    //  //return View(p);
+    //}
+
+    //[HttpPost]
+    //[HttpGet]
+    //[CustomValidation]
+    //[Authorize]
+    //public ActionResult Default(string id)
+    //{
+    //  if (ModelState.IsValid)
+    //  {
+    //    var p = new Person() { Name = "class" };
+    //    return View("Index", p);
+    //  }
+
+    //  return View("Index", null);
+    //}
+
+    //[HttpGet]
+    //[CustomException]
+    //public ActionResult Default()
+    //{
+    //  var p = new Person() { Name = "class" };
+    //  return View("Index", p);
+    //}
+
+    [HttpPost]
+    public ActionResult Validate(Person p)
+    {
+      if (ModelState.IsValid)
+      {
+        return View("Success");
+      }
+
+      return View("Failure");
+    }
+
+    [HttpGet]
+    public ActionResult Validate()
+    {
+      return View();
+    }
+  }
+}
---
+++ WebApp/WebApp.Mvc/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Mvc.Models
+{
+  public class Person
+  {
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Text)]
+    public string GivenName { get; set; }
+
+    [StringLength(50)]
+    public string FamilyName { get; set; }
+    //public List<Person> People { get; set; }
+    //public Address MyProperty { get; set; }
+  }
+}
